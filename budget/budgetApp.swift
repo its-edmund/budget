7b9106647468657a40da84ea9ea1024dca585311
Edmund Xin <edmundxin02@gmail.com>
2024-05-12 14:33:41 -0700

    Initial Commit

---
+++ budget/budgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetApp.swift
+//  budget
+//
+//  Created by Edmund Xin on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct budgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
